4b2ca5d7eabde3cf2cb3b955c8f54fa49701925a
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-05-19 20:22:20 +0700

    lavender: NusantaraProject
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -9,8 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Nusantara stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Inherit game space
+$(call inherit-product-if-exists, packages/apps/GameSpace/gamespace.mk)
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+USE_PIXEL_CHARGING := true
+# Device can use Quick Tap feature
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,10 +28,8 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := nad_lavender
 PRODUCT_MODEL := Redmi Note 7
-
-DEVICE_MAINTAINER := Ratoriku, Hazama25
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
