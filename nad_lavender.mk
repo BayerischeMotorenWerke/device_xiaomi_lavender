3c3061894d8d019e08d1cde9b682de600002f2cd
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-11-11 15:02:02 +0700

    lavender: NusantaraProject
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

---
+++ nad_lavender.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Nusantara stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+USE_PIXEL_CHARGING := true
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+
+# Inherit from lavender device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := nad_lavender
+PRODUCT_DEVICE := lavender
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := lavender
